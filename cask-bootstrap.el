fc64e01bc001309a5f914861ce3b753ced50b332
dickmao <none>
2021-02-14 08:44:28 -0500

    Revert "Restore TLS exception for <=24.4"
    This reverts commit a37efbe30b07fd94f9d6fad4b82697aa9a7d7649.

--- cask-bootstrap.el
+++ cask-bootstrap.el
@@ -50,27 +50,24 @@
       (package-user-dir cask-bootstrap-dir)
       (deps '(s dash f commander git epl shut-up cl-lib cl-generic
                 package-build eieio ansi)))
-  (when (version< emacs-version "24.5")
+  (when (version< emacs-version "25.1")
     ;; Builtin gnutls on Emacs 24 was used incorrectly, and
     ;; cannot connect to melpa.  Use external openssl instead.
     (require 'tls)
     (defvar tls-program)
     (setq tls-program '("openssl s_client -connect %h:%p -no_ssl3 -no_ssl2 -ign_eof"))
-    (defun gnutls-available-p () nil))
+    (defun gnutls-available-p () nil)
 
-  (when (version< emacs-version "25.1")
     (unless package--initialized
       (package-initialize))
 
     ;; install cl-lib depends by `package-build'.
-
     (unless (require 'cl-lib nil 'no-error)
       (package-refresh-contents)
       (package-install 'cl-lib))
 
     ;; Use vendored package-build package (and package-recipe)
     ;; because its newer versions require Emacs25.1+
-
     (require 'package-recipe (expand-file-name "package-recipe-legacy" cask-directory))
     (require 'package-build (expand-file-name "package-build-legacy" cask-directory))
     (delq 'cl-lib deps)
